62e2b09023fd46a92cf5975eb05a13d66e9a444e
Scott MacDonald <scottmacd88@yahoo.com>
2020-07-19 06:53:36 -0700

    Added two more repositories to Project Master

--- src/components/ProjectMaster.js
+++ src/components/ProjectMaster.js
@@ -14,26 +14,32 @@ class ProjectMaster extends React.Component {
                 title: "Trivia With Friends",
                 github: "https://github.com/dazacher/Trivia-With-Friends",
                 live: "https://github.com/dazacher/Trivia-With-Friends",
-                description: "Group Project Game"
+                description: "Group Project Trivia Game"
             },
             {
-                title: "Password Generator",
-                github: "https://github.com/BigMacD-WebDesign/Password_Generator",
-                live: "https://bigmacd-webdesign.github.io/Password_Generator/",
-                description: "Secure Password Generator"
-            }
+                title: "React Employee Directory",
+                github: "https://github.com/BigMacD-WebDesign/React_Employee_Directory",
+                live: "https://bigmacd-webdesign.github.io/React_Employee_Directory/",
+                description: "React Employee directory that can sort in alphabetical order on names, e-mail and sort via phone numbers."
+            },
+            {
+                title: "React Employee Directory",
+                github: "https://github.com/olegaposh/my_cost_tracker",
+                live: "https://my-cost-tracker.herokuapp.com/login",
+                description: "Group Project Budget Tracker."
+            },
         ]
     }
 
-    render () {
+    render() {
         return (
             <div id="project">
                 {this.state.projects.map((project, key) => (
-                    <Project 
+                    <Project
                         title={project.title}
                         github={project.github}
                         live={project.live}
-                        description={project.description} 
+                        description={project.description}
                         key={key} />
                 ))}
             </div>

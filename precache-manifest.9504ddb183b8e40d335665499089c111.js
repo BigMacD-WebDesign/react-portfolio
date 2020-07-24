15041c18d65919afc9c1fe25d964fd9b69df1905
Scott MacDonald <scottmacd88@yahoo.com>
2020-07-23 18:18:27 -0700

    Updates

---
+++ precache-manifest.9504ddb183b8e40d335665499089c111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258c678d54f79b3d155f354e3e6363e6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "604d39b80a0f655430fb",
+    "url": "/react-portfolio/static/css/main.03bb0534.chunk.css"
+  },
+  {
+    "revision": "ee5af798be051ec3a174",
+    "url": "/react-portfolio/static/js/2.49de80ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.49de80ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604d39b80a0f655430fb",
+    "url": "/react-portfolio/static/js/main.9c2d15be.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
